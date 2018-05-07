bac73e7ea11a5562b91feb531b4edac98b4a8654
jsiilva <jsiilva@outlook.com.br>
2018-05-07 20:57:57 -0300

    Primeiro commit. Iniciando a estrutura do projeto.

---
+++ src/br/com/faculdadersa/ivaniciojunior/ClientesEstrutura.java
@@ -0,0 +1,32 @@
+package br.com.faculdadersa.ivaniciojunior;
+
+public class ClientesEstrutura {
+
+    String nome;
+    String cpf;
+    String data;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
