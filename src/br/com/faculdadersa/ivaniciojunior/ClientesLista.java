f48ff2714e2b55f5436f9a7cb54fa59d618d59b9
jsiilva <jsiilva@outlook.com.br>
2018-05-08 11:46:55 -0300

    Código documentado e pronto para envio.

--- src/br/com/faculdadersa/ivaniciojunior/ClientesLista.java
+++ src/br/com/faculdadersa/ivaniciojunior/ClientesLista.java
@@ -6,11 +6,34 @@ import java.util.LinkedList;
 
 public class ClientesLista {
 
+    /*
+    *
+    * Instancia o objeto das listas de forma global
+    *
+    * lista: lista que armazena os nomes dos clientes no sistema de proteção ao crédito
+    * listaReincidentes: lista que armazena os clientes reincidentes de proteção ao crédito
+    *
+    * */
     private static LinkedList<ClientesEstrutura> lista = new LinkedList<ClientesEstrutura>();
     private static LinkedList<ClientesEstrutura> listaReincidentes = new LinkedList<ClientesEstrutura>();
 
+    /*
+     *
+     * Procedimento para adicionar um cliente na lista
+     *
+     * @arguments: String nome, String cpf
+     * @return: void
+     *
+     * */
     public void adicionaCliente (String nome, String cpf){
 
+        /*
+         *
+         * A cada invocação do procedimento adicionaCliente, instancia-se o nossa estrutura para cada cliente adicionado
+         *
+         * Obtem a data da inclusão (pegando a data atual do sistema)
+         *
+         * */
         ClientesEstrutura clientes = new ClientesEstrutura();
         Date data = new Date();
         SimpleDateFormat dataFormat = new SimpleDateFormat("dd/MM/yyyy");
@@ -27,10 +50,27 @@ public class ClientesLista {
         }
     }
 
+    /*
+     *
+     * Método que remove um cliente da lista
+     *
+     * @arguments: String cpf
+     * @return: boolean
+     *
+     * */
     public boolean removerCliente (String cpf){
 
+        // Variável auxiliar para controlar o retorno do método
         boolean retorno = false;
 
+        /*
+         *
+         * Percorre toda a lista buscando uma igualdade entre o CPF que está na iteração com o cpf do argumento da função, caso encontre, adiciona o cliente corrente na lista de reincidentes e, em seguida, o remove... ulterior a isso, a função passa o retorno para true e breca o laço pois não há mais necessidade de procurar.
+         *
+         * @arguments: String nome, String cpf
+         * @return: void
+         *
+         * */
         for (int i = 0; i < lista.size(); i++){
             if (lista.get(i).getCpf().equals(cpf)){
                 listaReincidentes.add(lista.get(i));
@@ -42,19 +82,30 @@ public class ClientesLista {
         return retorno;
     }
 
+    /*
+     *
+     * Procedimento para pesquisar um cliente na lista
+     *
+     * @arguments: String cpf
+     * @return: void
+     *
+     * */
     public void pesquisarCliente (String cpf){
 
+        // Variável auxiliar para controlar o retorno do método
         boolean encontrado = false;
         int indice = 0; // variável auxiliar que armazena o índice corrente do cliente encontrado
 
+        // percorre a lista
         for (int i = 0; i < lista.size(); i++){
-            if (lista.get(i).getCpf().equalsIgnoreCase(cpf)){
+            if (lista.get(i).getCpf().equals(cpf)){
                 encontrado = true;
-                indice = i;
+                indice = i; // armazena, em índice, o valor do índice da lista onde foi encontrado o termo da busca
                 break;
             }
         }
 
+        // se encontrado...
         if (encontrado) {
             System.out.println("\nCliente encontrado:");
             System.out.println("---------------------------------------------");
@@ -62,39 +113,70 @@ public class ClientesLista {
             System.out.println("CPF: " + lista.get(indice).getCpf());
             System.out.println("Data de inclusão: " + lista.get(indice).getData());
         } else
-            System.out.println("Cliente inexistente!");
+            System.out.println("\nCliente inexistente!");
     }
 
+    /*
+     *
+     * Método que verifica a existência de um cliente na lista
+     *
+     * @arguments: String cpf
+     * @return: void
+     *
+     * */
     public boolean verificaExistenciaCliente (String cpf){
 
+        // Variável auxiliar para controle de estado do laço
         boolean estado = false;
 
         for (int i = 0; i < lista.size(); i++){
-            if (lista.get(i).getCpf().equalsIgnoreCase(cpf)){
+            if (lista.get(i).getCpf().equals(cpf)){
                 estado = true;
             }
         }
         return estado;
     }
 
+    /*
+     *
+     * Procedimento que verifica se um cliente é reincidente na lista de proteção ao crédito
+     *
+     * @arguments: String cpf
+     * @return: void
+     *
+     * */
     public void verificaReincidentes (String cpf){
 
+        // Variáveis de controle de estado do laço
         boolean estado = false;
         int indice = 0;
 
+        /*
+         *
+         * Percorre de reincidentes buscando uma igualdade entre o CPF de quem está nesta lista e o CPF passado no argumento da função
+         *
+         * */
         for (int i = 0; i < listaReincidentes.size(); i++){
             if (listaReincidentes.get(i).getCpf().equals(cpf)){
                 estado = true;
-                indice = i;
+                indice = i; // armazena, em índice, o valor do índice da lista onde foi encontrado o termo da busca
                 break;
             }
         }
 
         if (estado){
-            System.out.println("O cliente "+listaReincidentes.get(indice).getNome()+" já esteve anteriormente na lista de proteção ao crédito, portanto, o seu CPF de número "+listaReincidentes.get(indice).getCpf()+" será cancelado!");
+            System.out.println("\nO cliente "+listaReincidentes.get(indice).getNome()+" já esteve anteriormente na lista de proteção ao crédito, portanto, o seu CPF de número "+listaReincidentes.get(indice).getCpf()+" será cancelado!");
         }
     }
 
+    /*
+     *
+     * Método para exibir os clientes na lista de proteção ao crédito
+     *
+     * @arguments: null
+     * @return: String
+     *
+     * */
     public String toString (){
 
         StringBuilder s = new StringBuilder();
--- src/br/com/faculdadersa/ivaniciojunior/ClientesListaTeste.java
+++ src/br/com/faculdadersa/ivaniciojunior/ClientesListaTeste.java
@@ -5,21 +5,32 @@ public class ClientesListaTeste {
     public static void main(String[] args) {
         ClientesLista clientes = new ClientesLista();
 
-        System.out.println("Lista de clientes no Sistema de proteção ao crédito:");
-        System.out.println("---------------------------------------------------------------\n");
-
+        // Adiciona alguns clientes na lista
         clientes.adicionaCliente("Ivanicio Jr", "07380242369");
         clientes.adicionaCliente("Lara Rodrigues", "123123123123");
         clientes.adicionaCliente("Fulano de Tal", "321321321321");
+        clientes.adicionaCliente("Fulano de Tal 2", "7851531448646");
+        clientes.adicionaCliente("Fulano de Tal 3", "1524999334886");
 
+        System.out.println("Lista de clientes no Sistema de proteção ao crédito:");
+        System.out.println("---------------------------------------------------------------\n");
+
+        // Exibe
         System.out.println(clientes.toString());
 
+        // Remove um pelo CPF
         if (clientes.removerCliente("123123123123"))
+            System.out.println("Removendo cliente...");
             System.out.println("Cliente removido com sucesso!");
 
+        // Adiciona o que foi removido novamente
         clientes.adicionaCliente("Lara Rodrigues", "123123123123");
 
+        // Testa se o sistema vai emitir a mensagem de cancelamento do CPF
         clientes.verificaReincidentes("123123123123");
+
+        // Pesquisando cliente
+        clientes.pesquisarCliente("07380242369");
     }
 
 }
